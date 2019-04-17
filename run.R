341dca24ff9497779ac589a7343b78b18c9091ba
gcgibson <gcgibson@ssid-umass-vl920-172-30-17-77.wireless.umass.edu>
2019-04-17 15:51:49 -0400

    first commit

---
+++ run.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(ForecastFramework)
+#theme_set(theme_bw())
+library(reticulate)
+use_python("/Users/gcgibson/anaconda/bin/python2.7")
+
+
+source_python("main.py")
+
+data <- "./training_data.txt"
+adjacency_matrix <- "./GER_states_adjacency.txt"
+epochs <- 100
+train_size <- 0.6
+valid_size <- 0.2
+save_name <- "tmp"
+model_name <- "CNNRNN_Res"
+#ext_data <- Nick puts in test data
+#ext_data_outcome <- Nick puts in true test data
+
+test.data <- train_dl(data,adjacency_matrix,train_size,valid_size,model_name,save_name)
+
+
+forecasts <- test(data,adjacency_matrix,train_size,valid_size,model_name,save_name,NULL,NULL)
